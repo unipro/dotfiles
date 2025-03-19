91ec3a999e2bf4efbe06ca24ae6059ee0b450f03
Byungwan Jun <bw.jun@cj.net>
2025-03-19 21:44:33 +0900

    feat: include .gitconfig.local if Git version < 2.43.0

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+version_lt() {
+    [ "$(printf '%s\n' "$1" "$2" | sort -V | head -n1)" = "$1" ]
+}
+
 # List of dotfiles to manage
 DOTFILES=(
     ".gitconfig"
@@ -29,10 +33,20 @@ if [ -f "$HOME/.bashrc" ]; then
     fi
 fi
 
+GIT_VERSION=$(git --version | awk '{print $3}')
+REQUIRED_VERSION="2.43.0"
+
 # Ensure local git configuration is sourced
 if [ ! -f "$HOME/.gitconfig.local" ]; then
     echo "Creating an empty .gitconfig.local for system-specific settings"
     touch "$HOME/.gitconfig.local"
+fi
+
+if version_lt "$GIT_VERSION" "$REQUIRED_VERSION"; then
+    cat "$HOME/.gitconfig.local" >> "$HOME/.gitconfig"
+else
+    echo -e "\n#\n# An additional Git configuration file on the local machine.\n#" >> "$HOME/.gitconfig"
+    echo -e "[includeIf \"exists:~/.gitconfig.local\"]\n\tpath = ~/.gitconfig.local" >> "$HOME/.gitconfig"
 fi
 
 echo "Dotfiles installation complete!"
